ab912cae6bbc082dd9612c74dfba21aa4bacf161
oninonins <varelinobubadibako11@gmail.com>
2024-09-30 18:29:28 +0700

    rework

--- PemilihanBilangan05.java
+++ PemilihanBilangan05.java
@@ -1,21 +1,17 @@
 import java.util.Scanner;
 
-
 public class PemilihanBilangan05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Masukan sebuah angka : ");
-        int angka = input.nextInt() ;
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.print("Angka " + angka + "termasuk bilangan genap " );
-        }
-        else {
-            System.out.print("Angka " + angka + "termasuk bilangan ganjil " );
-        }
+        
+        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
 
+        System.out.println("Angka " + angka + " termasuk bilangan " + hasil + ".");
+
+       
     }
- 
-    
 }
