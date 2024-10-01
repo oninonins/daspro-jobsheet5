82cec075e0b10284261894e989df9fd4ec16786a
oninonins <varelinobubadibako11@gmail.com>
2024-10-01 22:10:13 +0700

    revisi

--- tugas2.java
+++ tugas2.java
@@ -4,41 +4,29 @@ public class tugas2 {
     public static void main(String[] args) {
     
     Scanner input = new Scanner(System.in);
-    
-    boolean keanggotaan;
-    int jmlKopi, jmlTeh, jmlRoti;
-    double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, nominalBayar;
-    float diskon = 10 / 100f;
+    System.out.println("masukan usia anda : ");
+    if (input.hasNextInt()) {
+        int usia = input.nextInt();
 
-    System.out.print("Masukan keanggotaan (true/false): ");
-    keanggotaan = input.nextBoolean();
-    System.out.print("Masukan jumlah pembelian kopi: ");
-    jmlKopi = input.nextInt();
-    System.out.print("Masukan jumlah pembelian teh: ");
-    jmlTeh = input.nextInt();
-    System.out.print("Masukan jumlah pembelian roti: ");
-    jmlRoti = input.nextInt();
+        if (usia >= 0) {
+            if (usia <= 12) {
+                System.out.println("kategori usia anda anak ");                
+            } else if (usia <= 19) {
+                System.out.println("Usia anda remaja");
+            } else if (usia <= 64) {
+                System.out.println("kategori usia anda dewasa");
+            } else if (usia >= 65) {
+                System.out.println("kategori usia anda lansia");
+            }
 
-    totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
-    nominalBayar = totalHarga - (diskon * totalHarga);
-    byte totalByte = (byte) totalHarga;
-    int nominalInt = (int) nominalBayar;
-
-    if (keanggotaan) {
-            nominalBayar = totalHarga - (diskon * totalHarga);
         } else {
-            nominalBayar = totalHarga;
+            System.out.println("Input tidak valid");
         }
         
+    } else {
+        System.out.println("input tidak valid ");
+    }
 
-    System.out.println("keanggotaan pelanggan" + keanggotaan);
-    System.out.println("item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti"); 
-    System.out.println("Nominal bayar (int): Rp " + nominalInt);
-    System.out.println("Total harga (byte): Rp " + totalByte);
-
-
-    System.out.println("Total harga: " + totalHarga);
-    System.out.println("Nominal yang harus dibayar: " + nominalBayar);
     }
 }
  
